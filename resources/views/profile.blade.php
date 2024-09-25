b2df483556b14680f5f8c6d44f84214d4e182436
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-25 16:20:13 +0300

    Add first name and last name instead name

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -9,6 +9,15 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 space-y-6">
             <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
                 <div class="max-w-xl">
+                    <a class="inline-flex items-center px-4 py-2 bg-gray-800 border border-transparent rounded-md font-semibold text-xs text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150"
+                        href="{{ route('invoice') }}">
+                        Show my invoice
+                    </a>
+                </div>
+            </div>
+
+            <div class="p-4 sm:p-8 bg-white shadow sm:rounded-lg">
+                <div class="max-w-xl">
                     <livewire:profile.update-profile-information-form />
                 </div>
             </div>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InvoiceController;
 
 Route::view('/', 'welcome');
 
@@ -11,5 +12,7 @@ Route::view('dashboard', 'dashboard')
 Route::view('profile', 'profile')
     ->middleware(['auth'])
     ->name('profile');
+
+Route::get('/invoice', [InvoiceController::class, 'showinvoice'])->middleware(['auth'])->name('invoice');
 
 require __DIR__.'/auth.php';
