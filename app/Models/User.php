894fd129d8f3a7027dd182fd626e987a1fa621f9
Volodymyr Honcharov <ladimirvm87@gmail.com>
2024-09-25 23:03:18 +0300

    Change access modifier from public to protected for accessor fullName in User model

--- app/Models/User.php
+++ app/Models/User.php
@@ -47,7 +47,7 @@ class User extends Authenticatable
         ];
     }
 
-    public function fullName(): Attribute
+    protected function fullName(): Attribute
     {
         return Attribute::make(
             get: fn() => "$this->first_name $this->last_name"
